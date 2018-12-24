f4f1bc3e52825738756ecc6f7bad307ff94403d2
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-24 09:35:01 +0100

    Next ticket, restartCount and save file in class TicketControl

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,29 +1,56 @@
 const fs = require('fs');
 
 class TicketControl {
+
     constructor() {
+
         this.last = 0; // Last ticket i gave
         this.today = new Date().getDate();
 
         let data = require('../data/data.json');
         // Every new day I will restart the queue system
         // If the day of the data is the same as today we continue where the data ended
+
         if (data.today === this.today) {
 
+            this.last = data.last;
+
         } else {
+
             this.restartCount();
+
         }
     }
 
+    next() {
+
+        this.last += 1;
+        this.saveFile();
+
+        return `Ticket ${this.last}`
+
+    }
+
     restartCount() {
-        // We will save data once we restart
+
+        this.last = 0;
+        console.log('System has been initialized');
+
+        this.saveFile();
+
+    }
+
+    saveFile() {
+
         let jsonData = {
             last: this.last,
             today: this.today
         };
+
         let jsonDataString = JSON.stringify(jsonData);
+
         fs.writeFileSync('./server/data/data.json', jsonDataString);
-        console.log('System has been initialized');
+
     }
 }
 
